f1896c0286a7c8cc6fabca7fa980604f2031e24a
Greedysky <greedysky@163.com>
2022-06-01 10:42:23 +0800

    refactor: Optimize input module

--- ayflyhelper.cpp
+++ ayflyhelper.cpp
@@ -33,11 +33,10 @@ bool AyflyHelper::initialize()
         return false;
     }
 
-    const qint64 size = file.size();
-    const QByteArray &module = file.readAll();
+    const QByteArray &buffer = file.readAll();
     file.close();
 
-    if(!ay_initsongindirect((unsigned char *)module.constData(), sampleRate(), size))
+    if(!ay_initsongindirect((unsigned char *)buffer.constData(), sampleRate(), buffer.length()))
     {
         qWarning("AyflyHelper: ay_initsongindirect error");
         return false;
