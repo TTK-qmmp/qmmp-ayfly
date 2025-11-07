816a2a7459c1205e7e90b95c13479c1ab9214e06
Greedysky <greedysky@163.com>
2025-11-07 15:19:28 +0800

    feat: Support for qmmp 0.x Qt4

--- decoderayflyfactory.cpp
+++ decoderayflyfactory.cpp
@@ -106,3 +106,8 @@ QString DecoderAyflyFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(alfly, DecoderAyflyFactory)
+#endif
